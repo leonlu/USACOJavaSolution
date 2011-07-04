a22614442c635ee2084a3e5f1d7323c4d5c9b919
Leon Lu <leonluchen@hotmail.com>
2011-07-04 20:21:31 +0800

    friday.java update

--- USACOSection1/src/friday.java
+++ USACOSection1/src/friday.java
@@ -1,4 +1,4 @@
-// USACO Section 1.1
+// USACO Section 1.1.3
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,67 +8,81 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /*
-ID: leonluc1
-LANG: JAVA
-PROG: friday
-*/
+ ID: leonluc1
+ LANG: JAVA
+ PROG: friday
+ */
 
 public class friday {
-	 public static void main (String [] args) throws IOException {
-		 BufferedReader in = new BufferedReader(new FileReader("friday.in"));
-		 PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
-		 
-		 int N = Integer.parseInt(in.readLine());		 
-		 int[] sum13thOnWeekday = new int[7];
-		 int last13th = 0;
-		 
-		 for(int year = 1900; year < 1900 + N; year++){
-			 for(int month = 1; month <= 12; month++){
-				 if(year == 1900 && month == 1)
-					 last13th = 6;
-				 else{
-					 last13th = (next13thDaysAdd(month,year) + last13th) %7;
-				 }
-				 sum13thOnWeekday[last13th]++;
-			 }
-		 }
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader("friday.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"friday.out")), true);
 
-		 out.print(sum13thOnWeekday[6]+ " ");
-		 out.print(sum13thOnWeekday[0]+ " ");
-		 out.print(sum13thOnWeekday[1]+ " ");
-		 out.print(sum13thOnWeekday[2]+ " ");
-		 out.print(sum13thOnWeekday[3]+ " ");
-		 out.print(sum13thOnWeekday[4]+ " ");
-		 out.println(sum13thOnWeekday[5]);
-		 
-		 out.close();
-		 System.exit(0);
-	 }
-	 
-	 private static boolean leapYear(int year){
-		 if(year % 4 != 0) return false;
-		 if(year % 100 == 0)
-			 if(year % 400 == 0) return true;
-			 else return false;
-		 return true;
-	 }
-	 
-	 private static int next13thDaysAdd(int month, int year){
-		 switch(month){
-		 case 2: return 31;
-		 case 3: return leapYear(year) ? 29 :28;
-		 case 4: return 31;
-		 case 5: return 30;
-		 case 6: return 31;
-		 case 7: return 30;
-		 case 8: return 31;
-		 case 9: return 31;
-		 case 10: return 30;
-		 case 11: return 31;
-		 case 12: return 30;
-		 case 1: return 31;
-		 }
-		 // can't reach here
-		 return 0;
-	 }
+		int N = Integer.parseInt(in.readLine());
+		// 0:Sunday 1:Monday ... 5:Friday 6:Saturday
+		int[] day13thCount = new int[7];
+		int current13th = 0;
+
+		// for every year, every month
+		for (int year = 1900; year < 1900 + N; year++) {
+			for (int month = 1; month <= 12; month++) {
+				if (year == 1900 && month == 1)
+					current13th = 6;
+				else 
+					current13th = (next13thNeedAddDays(month, year) + current13th) % 7;
+				day13thCount[current13th]++;
+			}
+		}
+
+		// output 6:Saturday 0:Sunday ... 5:Friday
+		out.print(day13thCount[6] + " ");
+		for (int i = 0; i <= 4; i++)
+			out.print(day13thCount[i] + " ");
+		out.println(day13thCount[5]);
+
+		System.exit(0);
+	}
+
+	private static boolean leapYear(int year) {
+		if (year % 4 != 0)
+			return false;
+		if (year % 100 == 0)
+			if (year % 400 == 0)
+				return true;
+			else
+				return false;
+		return true;
+	}
+
+	private static int next13thNeedAddDays(int month, int year) {
+		switch (month) {
+		case 2:
+			return 31;
+		case 3:
+			return leapYear(year) ? 29 : 28;
+		case 4:
+			return 31;
+		case 5:
+			return 30;
+		case 6:
+			return 31;
+		case 7:
+			return 30;
+		case 8:
+			return 31;
+		case 9:
+			return 31;
+		case 10:
+			return 30;
+		case 11:
+			return 31;
+		case 12:
+			return 30;
+		case 1:
+			return 31;
+		}
+		// can't reach here
+		return 0;
+	}
 }

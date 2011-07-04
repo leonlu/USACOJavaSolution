44892691c809fb78ef7b8049725243df8be61aab
Leon Lu <leonluchen@hotmail.com>
2011-07-04 12:29:49 +0800

    gift1.java update

--- USACOSection1/src/gift1.java
+++ USACOSection1/src/gift1.java
@@ -1,4 +1,4 @@
-// USACO Section 1.1
+// USACO Section 1.1.2
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,41 +10,51 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 /*
-ID: leonluc1
-LANG: JAVA
-PROG: gift1
-*/
+ ID: leonluc1
+ LANG: JAVA
+ PROG: gift1
+ */
 
 public class gift1 {
-	  public static void main (String [] args) throws IOException {
-		    BufferedReader in = new BufferedReader(new FileReader("gift1.in"));
-		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out")));
-		    int cnt = Integer.parseInt(in.readLine());
-		    HashMap<String,Integer> map = new HashMap<String,Integer>();
-		    String[] orderedNames = new String[cnt];
-		    for(int i = 0; i < cnt; i ++){
-		    	orderedNames[i] = in.readLine();
-		    	map.put(orderedNames[i],0);
-		    }
-		    for(int i = 0; i < cnt; i++){
-		    	String name = in.readLine();
-		    	StringTokenizer st = new StringTokenizer(in.readLine());
-		    	int money = Integer.parseInt(st.nextToken());
-		    	int giveCount = Integer.parseInt(st.nextToken());
-		    	if(giveCount == 0) continue;
-		    	int remainMoney = money % giveCount;
-		    	int givePerMoney = money / giveCount;
-		    	map.put(name, map.get(name).intValue() - (money - remainMoney));
-		    	for(int j = 0; j < giveCount; j++){
-		    		String name2 = in.readLine();
-		    		map.put(name2, map.get(name2).intValue() + givePerMoney);
-		    	}
-		    }
-		    
-		    for(String s : orderedNames){
-		    	out.println(s + " " + map.get(s));
-		    }
-		    out.close();
-		    System.exit(0);
-	  }
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader("gift1.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"gift1.out")), true);
+
+		// initialization
+		int cnt = Integer.parseInt(in.readLine());
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		String[] names = new String[cnt];
+		for (int i = 0; i < cnt; i++) {
+			names[i] = in.readLine();
+			map.put(names[i], 0);
+		}
+		
+		// process
+		for (int i = 0; i < cnt; i++) {
+			String name = in.readLine();
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			int money = Integer.parseInt(st.nextToken());
+			int giveCount = Integer.parseInt(st.nextToken());
+			if (giveCount == 0)
+				continue;
+			
+			// giver
+			int remainMoney = money % giveCount;
+			int givePerMoney = money / giveCount;
+			map.put(name, map.get(name).intValue() - (money - remainMoney));
+			
+			// receiver
+			for (int j = 0; j < giveCount; j++) {
+				String receiver = in.readLine();
+				map.put(receiver, map.get(receiver).intValue() + givePerMoney);
+			}
+		}
+
+		// output
+		for (String s : names)
+			out.println(s + " " + map.get(s));
+
+		System.exit(0);
+	}
 }

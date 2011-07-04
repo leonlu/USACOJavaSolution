98f6381ab1a2a7ff96c69b5c6c97d7a56bfa5a08
Leon Lu <leonluchen@hotmail.com>
2011-07-04 11:52:47 +0800

    format update for ride.java

--- USACOSection1/src/ride.java
+++ USACOSection1/src/ride.java
@@ -1,10 +1,10 @@
 // USACO Section 1.1.1
 
 /*
-ID: leonluc1
-LANG: JAVA
-PROG: ride
-*/
+ ID: leonluc1
+ LANG: JAVA
+ PROG: ride
+ */
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -14,24 +14,25 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 class ride {
-	  public static void main (String [] args) throws IOException {
-		    BufferedReader in = new BufferedReader(new FileReader("ride.in"));
-		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")),true);
-		    String comet = in.readLine();
-		    String group = in.readLine();
-		    
-		    if(mod47(comet) == mod47(group))
-		    	out.println("GO");
-		    else
-		    	out.println("STAY");
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader("ride.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"ride.out")), true);
+		String comet = in.readLine();
+		String group = in.readLine();
 
-		    System.exit(0);
-	  }
-	  
-	  private static int mod47(String s){
-		  int res = 1;
-		  for(int i = 0; i < s.length(); i++)
-			  res *= (s.charAt(i)-'A'+1);
-		  return res % 47;
-	  }
+		if (mod47(comet) == mod47(group))
+			out.println("GO");
+		else
+			out.println("STAY");
+
+		System.exit(0);
+	}
+
+	private static int mod47(String s) {
+		int res = 1;
+		for (int i = 0; i < s.length(); i++)
+			res *= (s.charAt(i) - 'A' + 1);
+		return res % 47;
+	}
 }

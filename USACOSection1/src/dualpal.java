b1a9abdce8dac9797fad4f45e10ff5725c175c9a
Leon Lu <leonluchen@hotmail.com>
2011-07-08 16:03:47 +0800

    dualpal.java update
    Signed-off-by: Leon Lu <leonluchen@hotmail.com>

--- USACOSection1/src/dualpal.java
+++ USACOSection1/src/dualpal.java
@@ -1,4 +1,4 @@
-// USACO Section 1.2
+// USACO Section 1.2.5
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,64 +8,63 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-
 /*
-ID: leonluc1
-LANG: JAVA
-PROG: dualpal
-*/
+ ID: leonluc1
+ LANG: JAVA
+ PROG: dualpal
+ */
 
 public class dualpal {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("dualpal.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
-				"dualpal.out")));
+				"dualpal.out")), true);
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int S = Integer.parseInt(st.nextToken());
-		do{
-			if(dualPal(++S)){
+		do {
+			if (dualPal(++S)) {
 				out.println(S);
-				if(--N == 0) break;
+				if (--N == 0)
+					break;
 			}
-		}
-		while( S != Integer.MAX_VALUE);
-		out.close();
+		} while (S != Integer.MAX_VALUE);
 		System.exit(0);
 	}
-	
-	
-	private static char digitToChar(int digit){
-		return (char)(digit + '0');
+
+	private static char digitToChar(int digit) {
+		return (char) (digit + '0');
 	}
-	
-	private static boolean dualPal(int num){
+
+	private static boolean dualPal(int num) {
 		int count = 0;
 		int base = 2;
-		do{
-			if(isPal(toBaseForm(num,base))){
-				if(++count >= 2) return true;
+		do {
+			if (isPal(toBaseForm(num, base))) {
+				if (++count >= 2)
+					return true;
 			}
-		}while( ++base <= 10);
+		} while (++base <= 10);
 		return false;
 	}
-	
-	private static String toBaseForm(int num, int base){
+
+	private static String toBaseForm(int num, int base) {
 		StringBuilder sb = new StringBuilder();
-		do{
+		do {
 			char curChar = digitToChar(num % base);
 			sb.append(curChar);
-		}while( (num /= base) !=0);
+		} while ((num /= base) != 0);
 		sb.reverse();
 		return sb.toString();
 	}
-	
-	private static boolean isPal(String s){
-		int low = 0, high = s.length()-1;
-		while(low < high){
-			if(s.charAt(low)!= s.charAt(high))
+
+	private static boolean isPal(String s) {
+		int low = 0, high = s.length() - 1;
+		while (low < high) {
+			if (s.charAt(low) != s.charAt(high))
 				return false;
-			low++;high--;
+			low++;
+			high--;
 		}
 		return true;
 	}

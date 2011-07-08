4c6cd705fec6459b29978769bf0339688d608e03
Leon Lu <leonluchen@hotmail.com>
2011-07-08 15:14:41 +0800

    update for palsquare.java

--- USACOSection1/src/palsquare.java
+++ USACOSection1/src/palsquare.java
@@ -1,4 +1,4 @@
-// USACO Section 1.2
+// USACO Section 1.2.4
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,53 +7,53 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 /*
-ID: leonluc1
-LANG: JAVA
-PROG: palsquare
-*/
+ ID: leonluc1
+ LANG: JAVA
+ PROG: palsquare
+ */
 
 public class palsquare {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("palsquare.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
-				"palsquare.out")));
+				"palsquare.out")),true);
 		int base = Integer.parseInt(in.readLine());
-		for(int i = 1; i <= 300; i++){
-			String square = toBaseForm(i*i,base);
-			if(isPalsquare(square)){
-				out.print(toBaseForm(i,base));
+		for (int i = 1; i <= 300; i++) {
+			String square = toBaseForm(i * i, base);
+			if (isPalsquare(square)) {
+				out.print(toBaseForm(i, base));
 				out.print(" ");
 				out.println(square);
 			}
 		}
-		out.close();
 		System.exit(0);
 	}
-	
-	
-	private static char digitToChar(int digit){
-		if(digit < 10) return (char)(digit + '0');
-		else return (char)('A' -10 + digit);
+
+	private static char digitToChar(int digit) {
+		if (digit < 10)
+			return (char) (digit + '0');
+		else
+			return (char) ('A' - 10 + digit);
 	}
-	
-	private static String toBaseForm(int num, int base){
+
+	private static String toBaseForm(int num, int base) {
 		StringBuilder sb = new StringBuilder();
-		do{
+		do {
 			char curChar = digitToChar(num % base);
 			sb.append(curChar);
-		}while( (num /= base) !=0);
+		} while ((num /= base) != 0);
 		sb.reverse();
 		return sb.toString();
 	}
-	
-	private static boolean isPalsquare(String s){
-		int low = 0, high = s.length()-1;
-		while(low < high){
-			if(s.charAt(low)!= s.charAt(high))
+
+	private static boolean isPalsquare(String s) {
+		int low = 0, high = s.length() - 1;
+		while (low < high) {
+			if (s.charAt(low) != s.charAt(high))
 				return false;
-			low++;high--;
+			low++;
+			high--;
 		}
 		return true;
 	}

9b012128c91727fa89c2ac6b6c13007461b5e5e0
Leon Lu <leonluchen@hotmail.com>
2011-07-10 12:31:48 +0800

    barn1.java update

--- USACOSection1/src/barn1.java
+++ USACOSection1/src/barn1.java
@@ -1,4 +1,4 @@
-// USACO Section 1.3
+// USACO Section 1.3.2
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -20,31 +20,36 @@ public class barn1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("barn1.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
-				"barn1.out")));
+				"barn1.out")),true);
 		
+		// initialize
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		int M = Integer.parseInt(st.nextToken());
 		/*int S =*/ Integer.parseInt(st.nextToken());
 		int C = Integer.parseInt(st.nextToken());
 		int[] stallNos = new int[C];
-		for(int i = 0; i < stallNos.length; i++){
+		for(int i = 0; i < stallNos.length; i++)
 			stallNos[i] = Integer.parseInt(in.readLine());
-		}
+				
+		// sort by stall number
 		Arrays.sort(stallNos);
-		int[] distances = new int[C-1];
-		for(int i = 0; i < distances.length; i++){
-			distances[i]= stallNos[i+1] - stallNos[i] - 1;
-		}
-		Arrays.sort(distances);
-		int index = distances.length-1;
+		// calculate gaps and sort it
+		int[] gaps = new int[C-1];
+		for(int i = 0; i < gaps.length; i++)
+			gaps[i]= stallNos[i+1] - stallNos[i] - 1;
+		Arrays.sort(gaps);
+		// leave the most (M-1) biggest gaps behind by adding one more board each time 
+		int index = gaps.length-1;
 		index -= (M-1);
+		// calculate the small gaps that have to be covered without cows in it
 		int res = C;
 		while(index >=0){
-			res+= distances[index];
+			res+= gaps[index];
 			index --;
 		}
+		
+		// output
 		out.println(res);
-		out.close();
 		System.exit(0);
 	}
 }

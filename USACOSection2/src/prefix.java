8778663d1436d7eed1c6048929af84516ea2908f
Leon Lu <leonluchen@hotmail.com>
2011-07-28 14:52:58 +0800

    prefix.java update

--- USACOSection2/src/prefix.java
+++ USACOSection2/src/prefix.java
@@ -1,4 +1,4 @@
-// Section 2.3
+// Section 2.3.1
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,61 +9,55 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-
-
 /*
-ID: leonluc1
-PROG: prefix
-LANG: JAVA
-*/
+ ID: leonluc1
+ PROG: prefix
+ LANG: JAVA
+ */
 
-// backward dp, like the triangle
-
-// f(x) = longest substring to the end at x,
-// for every prefix that matches substring beginning at x, figure out the f(x) = max(lengthOfSub + f(x+lengthOfSub))
+// DP: f(x) = longest substring length beginning at x.
+// for every prefix that matches substring beginning at x
+// f(x) = max(prefixlength + f(x+prefixlength))
 public class prefix {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new FileReader("prefix.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("prefix.out")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"prefix.out")), true);
 		List<String> prefix = new ArrayList<String>();
 
+		// read in
 		String tmpStr;
-		while(!(tmpStr = in.readLine()).equals(".")){
+		while (!(tmpStr = in.readLine()).equals(".")) {
 			StringTokenizer st = new StringTokenizer(tmpStr);
-			while(st.hasMoreTokens())
+			while (st.hasMoreTokens())
 				prefix.add(st.nextToken());
 		}
 		StringBuilder sb = new StringBuilder();
-		while((tmpStr = in.readLine()) != null){
+		while ((tmpStr = in.readLine()) != null) {
 			sb.append(tmpStr);
 		}
 		String s = sb.toString();
 		int[] length = new int[s.length()];
-		
-		for(int i = s.length()-1; i>=0;i--){
-			int tmpMaxLen = 0;
-			label:
-			for(String p : prefix){
-				// don't use if(s.substring(i).indexOf(p)==0): TLE!
-				boolean flag = false;
-				for(int j = 0; j < p.length() && i + j < s.length(); j++){
-					if(p.charAt(j) != s.charAt(i+j)){
+
+		// process
+		for (int i = s.length() - 1; i >= 0; i--) {
+			label: 
+			for (String p : prefix) {
+				for (int j = 0; j < p.length() && i + j < s.length(); j++) {
+					if (p.charAt(j) != s.charAt(i + j))
 						continue label;
+					if (j == p.length() - 1) {
+						int pLen = p.length();
+						int t = (i + pLen) < s.length() ? pLen + length[i + pLen] : pLen;
+						length[i] = t > length[i] ? t : length[i];
 					}
-					if(j == p.length()-1) flag = true;
-				}
-				if(flag){
-					int pLen = p.length();
-					int t = (i + pLen) < s.length() ? pLen + length[i + pLen] : pLen;
-					tmpMaxLen = t > tmpMaxLen ? t: tmpMaxLen;
 				}
 			}
-			length[i] = tmpMaxLen;
 		}
-		
+
+		// output
 		out.println(length[0]);
 
-		out.close();
 		System.exit(0);
 	}
 }

91a812e34e791e47346aa2e54a8385c390ecce3b
Leon Lu <leonluchen@hotmail.com>
2011-07-29 13:23:35 +0800

    section 2.3 update

--- USACOSection2/src/money.java
+++ USACOSection2/src/money.java
@@ -1,4 +1,4 @@
-// Section 2.3
+// Section 2.3.4
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,42 +7,42 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-// dp : f(i,j) = f(i-1,j) + f(i, j-coin(i))
-
 /*
-ID: leonluc1
-PROG: money
-LANG: JAVA
-*/
+ ID: leonluc1
+ PROG: money
+ LANG: JAVA
+ */
 
+//dp : f(i,j) = f(i-1,j) + f(i, j-coin(i))
 public class money {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new FileReader("money.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("money.out")));
-		
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"money.out")), true);
+
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		int V = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
 
-		int[] coins = new int[V+1];
-		for(int i = 1; i <=V; i++){
-			if(!st.hasMoreTokens()) st = new StringTokenizer(in.readLine());
+		int[] coins = new int[V + 1];
+		for (int i = 1; i <= V; i++) {
+			if (!st.hasMoreTokens())
+				st = new StringTokenizer(in.readLine());
 			coins[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		long[][] dp = new long[V+1][N+1];	// long is a must
-		
-		for(int i = 0; i <=V; i++)
+
+		long[][] dp = new long[V + 1][N + 1]; // long is a must
+
+		for (int i = 0; i <= V; i++)
 			dp[i][0] = 1;
-		
-		for(int i = 1; i <= V; i++)
-			for(int j = 1; j <=N; j++){
-				dp[i][j] = dp[i-1][j] + (j - coins[i] >= 0? dp[i][j-coins[i]]:0);
-			}
-		
+
+		for (int i = 1; i <= V; i++)
+			for (int j = 1; j <= N; j++)
+				dp[i][j] = dp[i - 1][j]
+						+ (j - coins[i] >= 0 ? dp[i][j - coins[i]] : 0);
+
 		out.println(dp[V][N]);
-		
-		out.close();
+
 		System.exit(0);
 	}
 }
--- USACOSection2/src/nocows.java
+++ USACOSection2/src/nocows.java
@@ -1,4 +1,4 @@
-// Section 2.3
+// Section 2.3.2
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,33 +8,34 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 /*
-ID: leonluc1
-PROG: nocows
-LANG: JAVA
-*/
+ ID: leonluc1
+ PROG: nocows
+ LANG: JAVA
+ */
 
-
+// dp: f(n,k) = Sigma(f(i,k-1)*f(n-1-i,k-1)), i = 1,2,...n-2
 public class nocows {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new FileReader("nocows.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("nocows.out")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+				"nocows.out")), true);
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		int[][] dp = new int[N+1][K+1];
-		
-		for(int k = 1; k <= K; k++){
+		int[][] dp = new int[N + 1][K + 1];
+
+		for (int k = 1; k <= K; k++) {
 			dp[1][k] = 1;
-			for(int n = 1; n <= N; n+=2) //youhua: n++  ->  n+=2
-				for(int i = 1; i <= n-2; i++)
-					dp[n][k] = (dp[n][k] + dp[i][k-1] * dp[n-1-i][k-1]) % 9901;
+			// optimization: since n must be an odd number, n++ -> n+=2
+			for (int n = 1; n <= N; n += 2)
+				for (int i = 1; i <= n - 2; i++)
+					dp[n][k] = (dp[n][k] + dp[i][k - 1] * dp[n - 1 - i][k - 1]) % 9901;
 		}
-		
-		int res = (dp[N][K] - dp[N][K-1] + 9901) % 9901;
+
+		int res = (dp[N][K] - dp[N][K - 1] + 9901) % 9901;
 		out.println(res);
-		
-		out.close();
+
 		System.exit(0);
 	}
 }
